9c811e844c3f1efabcf77c7227ade4ba31ebda8e
beznavorotov <beznavorotov@gmail.com>
2024-03-22 18:06:08 +0200

    add task react

---
+++ src/Main.jsx
@@ -0,0 +1,72 @@
+import {useState, useEffect} from 'react';
+import List from './components/List';
+import { v4 as uuidv4 } from 'uuid';
+
+function Main(){
+
+    const [tasks,setTasks] = useState(()=>{
+        const storedTodos = localStorage.getItem('tasks');
+        if(!storedTodos){
+            return []
+        } else {
+            return JSON.parse(storedTodos);
+        }
+    });
+    const [tasksTitle, setTasksTitle] = useState('');
+
+    useEffect(()=>{
+        localStorage.setItem('tasks',JSON.stringify(tasks));
+    },[tasks])
+
+    const addTask = (e) => {
+        const storedTodos = JSON.parse(localStorage.getItem('tasks'));
+        if (e.key === 'Enter' && e.target.value !== ''){
+            setTasks([
+                ...storedTodos, {
+                    id: uuidv4(),
+                    title: tasksTitle,
+                    status: false
+                }
+            ]);
+            setTasksTitle('');
+        }
+    }
+
+      function change() {
+        const storedTodos = JSON.parse(localStorage.getItem('tasks'));
+        setTasks([
+                        ...storedTodos, {
+                            id: uuidv4(),
+                            title: tasksTitle,
+                            status: false
+                        }
+                    ]);
+        setTasksTitle('');
+      }
+
+
+    const date = new Date();
+    const monthNames = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    const month = monthNames[date.getMonth()];
+    const day = date.getDate();
+    const year = date.getFullYear();
+
+    return (
+        <div className='container'>
+            <h1>Note your tasks</h1>
+            <span>{month + ' ' + day + ', ' + year}</span>
+            <div className='input-filed'>
+                <input type='text' 
+                value={tasksTitle}
+                onChange={event => setTasksTitle(event.target.value)}
+                onKeyDown={addTask}
+                />
+                <label>Task name</label>
+                <button className='button' onClick={change}>Add Task</button>
+            </div>
+            <List tasks={tasks} />
+        </div>
+    );
+}
+
+export default Main;
---
+++ src/components/Item.jsx
@@ -0,0 +1,58 @@
+import {useState} from 'react';
+
+export default function Item({title, id, status}){
+
+    const [checked, setChecked] = useState(status);
+    const classes = ['todo'];
+
+
+    if (checked) {
+        classes.push('status');
+    }
+
+    const updateStatus = () => {
+        setChecked(!checked);
+        const storedTodos = JSON.parse(localStorage.getItem('tasks'));
+        storedTodos.map((el)=>{
+            if(el.id === id){
+                el.status = !checked;
+            }
+            return true
+        })
+        localStorage.setItem('tasks',JSON.stringify(storedTodos));
+    }
+
+    const [visible,setVisible] = useState(true);
+
+    const removeElement = () => {
+        setVisible(prev => !prev);
+        const storedTodos = JSON.parse(localStorage.getItem('tasks'));
+        let removeTodos = storedTodos.filter(item => {
+            if(item.id !== id){
+                return true
+            }
+            return false
+        });
+        localStorage.setItem('tasks',JSON.stringify(removeTodos));
+    }
+
+    return (
+        <>
+            {visible && (
+                <li className={classes.join(' ')}>
+                    <label>
+                        <input type="checkbox"
+                        checked={checked}
+                        onChange={updateStatus} />
+                        <span>{title}</span>
+                        <i className="material-icons red-text"
+                        onClick={removeElement}
+                        >
+                            X
+                        </i>
+                    </label>
+                </li>
+            )}
+        </>
+    )
+}
